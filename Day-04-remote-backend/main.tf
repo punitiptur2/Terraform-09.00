076c91c3d9aad5e880c372e99b5f9c0e80a5cf5f
punith <punitiptur2@gmail.com>
2026-06-03 20:34:49 +0530

    remote

--- Day-04-remote-backend/main.tf
+++ Day-04-remote-backend/main.tf
@@ -6,3 +6,8 @@ resource "aws_subnet" "name" {
   vpc_id     = aws_vpc.name.id
   cidr_block = "10.0.0.0/26"
 }
+
+resource "aws_subnet" "name2" {
+  vpc_id     = aws_vpc.name.id
+  cidr_block = "10.0.0.64/26"
+}
