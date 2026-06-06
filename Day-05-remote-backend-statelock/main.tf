7a46925e5ab243f571a849c94fd76098cd51d322
punith <punitiptur2@gmail.com>
2026-06-06 08:59:20 +0530

    day05

---
+++ Day-05-remote-backend-statelock/main.tf
@@ -0,0 +1,14 @@
+resource "aws_vpc" "name" {
+  cidr_block = "10.0.0.0/24"
+  tags = {
+    Name = "Terraform-09:00AM"
+  }
+}
+
+
+resource "aws_subnet" "name" {
+  vpc_id     = aws_vpc.name.id
+  cidr_block = "10.0.1.0/24"
+}
+
+
