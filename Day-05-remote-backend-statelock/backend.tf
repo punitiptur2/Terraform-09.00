88c0a689cbe9daccea8f0e9066d584385903bba1
punith <punitiptur2@gmail.com>
2026-06-06 10:51:23 +0530

    vs

--- Day-05-remote-backend-statelock/backend.tf
+++ Day-05-remote-backend-statelock/backend.tf
@@ -6,3 +6,5 @@ terraform {
     region = "us-east-1"
   }
 }
+
+#state lockfile : Terraform acquires a state lock to protect the state from being written by multiple users or processes at the same time. This is crucial for preventing conflicts and ensuring the integrity of the state file. When a user or process initiates an operation that modifies the state, Terraform will attempt to acquire a lock on the state file. If another user or process has already acquired the lock, Terraform will wait until the lock is released before proceeding with the operation. This mechanism
--- Day-05-remote-backend-statelock/main.tf
+++ Day-05-remote-backend-statelock/main.tf
@@ -2,7 +2,7 @@
 resource "aws_vpc" "name" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "Terraform-09:00AM"
+    Name = "Terraform-11:00AM"
   }
 }
 
